4ae63df233168a31074a9c55c46e9348982b05b0
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2020-02-07 16:00:27 +0800

    Add files via upload

--- CollectSamples.py
+++ CollectSamples.py
@@ -1,5 +1,45 @@
+from utils import *
+from os.path import join
+from multiprocessing import Pool
+import numpy as np
 data_root = 'data/'
 sr = 44100
 max_length = sr*32 # ignore samples longer than 32 seconds
 fixed_length = sr*32 # trim all samples to 32000 milliseconds
 limit = None # set this to 100 to only load the first 100 samples
+files = list(list_all_files(join(data_root, 'samples'), ['.wav']))
+len(files)
+def load_sample(fn, sr=None,
+                max_length=None, fixed_length=None, normalize=True):
+    if fn == '': # ignore empty filenames
+        return None
+    audio, _ = ffmpeg_load_audio(fn, sr, mono=True)
+    duration = len(audio)
+    if duration == 0: # ignore zero-length samples
+        return None
+    if max_length and duration >= max_length: # ignore long samples
+        return None
+    if fixed_length:
+        audio.resize(fixed_length)
+    max_val = np.abs(audio).max()
+    if max_val == 0: # ignore completely silent sounds
+        return None
+    if normalize:
+        audio /= max_val
+    return (fn, audio, duration)
+def job(fn):
+    return load_sample(fn, sr=sr,
+                       max_length=max_length, fixed_length=fixed_length)
+pool = Pool()
+results = pool.map(job, files[:limit])
+print 'Processed', len(results), 'samples'
+valid = filter(None, results)
+filenames = [x[0] for x in valid]
+samples = [x[1] for x in valid]
+durations = [x[2] for x in valid]
+samples = np.asarray(samples)
+np.savetxt(join(data_root, 'filenames.txt'), filenames, fmt='%s')
+np.savetxt(join(data_root, 'durations.txt'), durations, fmt='%i')
+np.save(join(data_root, 'samples.npy'), samples)
+print 'Saved', len(valid), 'samples'
+
--- analysis.py
+++ analysis.py
@@ -9,7 +9,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import librosa
 import numpy
 from multiprocessing import Pool
