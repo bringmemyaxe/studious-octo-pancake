49dce224519c76c40184486dc16c3d6c87350529
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2020-02-07 14:10:26 +0800

    Update CollectSamples.py

--- CollectSamples.py
+++ CollectSamples.py
@@ -1,5 +1,5 @@
 data_root = 'data/'
 sr = 44100
-max_length = sr*32 # ignore samples longer than 4 seconds
-fixed_length = sr*32 # trim all samples to 250 milliseconds
+max_length = sr*32 # ignore samples longer than 32 seconds
+fixed_length = sr*32 # trim all samples to 32000 milliseconds
 limit = None # set this to 100 to only load the first 100 samples
