a4dc58851a93473034dc8d2232c0819fc836534c
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2020-02-12 19:10:28 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -8,54 +8,59 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os
 import librosa
 import numpy
-from multiprocessing import Pool
-
-sample_rate = 44100
-
-    
-def scale(arr):
-    # get the average
-    avg = numpy.average(arr)
-    # scale from 20,20000 to 0,1
-    return (avg - 20) / (20000 - 20)
+ref_sounds = []
+for root, dirs, files in os.walk("./ref_sounds", topdown = False):
+    for name in files:
+        ref_sounds.append(os.path.join(root, name))
+for ref_sound in ref_sounds:
+    sample_rate = 44100
 
 
-def analyse(file):
-    print(file)
-    y, sr = librosa.load(file, sr=sample_rate, duration=0.25)
-    centroid = scale(librosa.feature.spectral_centroid(y=y, sr=sr))
-    bandwidth = scale(librosa.feature.spectral_bandwidth(y=y, sr=sr)) 
-    # rolloff = scale(librosa.feature.spectral_rolloff(y=y, sr=sr))   
-    # contrast = scale(librosa.feature.spectral_contrast(y=y, sr=sr))   
-    return numpy.asarray([centroid, bandwidth])
+    def scale(arr):
+        # get the average
+        avg = numpy.average(arr)
+        # scale from 20,20000 to 0,1
+        return (avg - 20) / (20000 - 20)
 
 
-kick_values = analyse("./ref_sounds/ref_sound1.wav")
-snare_values = analyse("./ref_sounds/ref_sound2.wav")
-hihat_values = analyse("./ref_sounds/ref_sound3.wav")
-open_values = analyse("./ref_sounds/ref_sound4.wav")
+    def analyse(file):
+        y, sr = librosa.load(file, sr=sample_rate, duration=0.25)
+        centroid = scale(librosa.feature.spectral_centroid(y=y, sr=sr))
+        bandwidth = scale(librosa.feature.spectral_bandwidth(y=y, sr=sr))
+        # rolloff = scale(librosa.feature.spectral_rolloff(y=y, sr=sr))
+        # contrast = scale(librosa.feature.spectral_contrast(y=y, sr=sr))
+        return numpy.asarray([centroid, bandwidth])
 
-def get_distance(test_values):
-    test_values = numpy.asarray(test_values)
-    kick_dist = numpy.linalg.norm(test_values - kick_values)
-    snare_dist = numpy.linalg.norm(test_values - snare_values)
-    hihat_dist = numpy.linalg.norm(test_values - hihat_values)
-    open_dist = numpy.linalg.norm(test_values - open_values)
-    return [kick_dist, snare_dist, hihat_dist, open_dist]
 
-# print get_distance(snare_values)
+    kick_values = analyse(ref_sound)
+    closest = 1
 
-def process(file_name):
-    return get_distance(analyse(file_name))
+    def get_distance(test_values):
+        global closest
+        global best_option
+        test_values = numpy.asarray(test_values)
+        kick_dist = numpy.linalg.norm(test_values - kick_values)
+        prepared_file_name, useless_extension = os.path.splitext(os.path.basename(sample_file_name))
+        if kick_dist < closest:
+            closest = kick_dist
+            best_option = prepared_file_name
+        return [kick_dist]
 
-if __name__ == "__main__":
+
+    # print get_distance(snare_values)
+
+    def process(file_name):
+        global sample_file_name
+        sample_file_name = file_name
+        return get_distance(analyse(file_name))
+
+
     with open("./data/filenames.txt") as file_names:
-        files = file_names.read().splitlines() 
-        limit = None
-        results = []
-        pool = Pool()
-        results = pool.map(process, files[:limit])
-        numpy.savetxt("data/analysis.tsv", results, delimiter="\t", fmt="%.6f")
+        files = file_names.read().splitlines()
+        for something in files:
+            process(something)
+    print(best_option, 'is the closest sample for', ref_sound)
+    print('The difference is:', closest)
