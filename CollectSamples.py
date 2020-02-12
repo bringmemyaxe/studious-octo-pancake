80b32497eb70c99c6735f56ac315d22425a91520
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2020-02-12 20:00:49 +0800

    Update CollectSamples.py

--- CollectSamples.py
+++ CollectSamples.py
@@ -4,8 +4,8 @@ from multiprocessing import Pool
 import numpy as np
 data_root = 'data/'
 sr = 44100
-max_length = sr*32 # ignore samples longer than 32 seconds
-fixed_length = sr*32 # trim all samples to 32000 milliseconds
+max_length = sr*4 # ignore samples longer than 4 seconds
+fixed_length = sr/4 # trim all samples to 250 milliseconds
 limit = None # set this to 100 to only load the first 100 samples
 files = list(list_all_files(join(data_root, 'samples'), ['.wav']))
 len(files)
